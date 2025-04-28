48ec36c5d94c9605e04a6d04edbddbb6632b981f
Benjamin Drung <benjamin.drung@canonical.com>
2025-04-28 15:45:33 +0200

    WIP: test: Use vcr to record/replay Launchpad's HTTP interactions
    TODO:
     * check credentials
     * reduce size of vcr files
    
    Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

--- tests/integration/test_crashdb_launchpad.py
+++ tests/integration/test_crashdb_launchpad.py
@@ -15,6 +15,7 @@
 import atexit
 import email
 import os
+import pathlib
 import shutil
 import subprocess
 import sys
@@ -27,15 +28,39 @@ from unittest.mock import MagicMock
 
 try:
     from launchpadlib.errors import HTTPError
+    from vcr import VCR
 
     IMPORT_ERROR = None
 except ImportError as error:
     IMPORT_ERROR = error
 
+    class VCRMock:
+        # pylint: disable=missing-function-docstring
+        """Mock the VCR class."""
+
+        @staticmethod
+        def ensure_suffix(suffix):
+            pass
+
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def use_cassette(self, *args, **kwargs):
+            pass
+
+    VCR = VCRMock
+
+
 from apport.crashdb_impl.launchpad import CrashDatabase
 from apport.packaging_impl import impl as packaging
 from apport.report import Report
 
+FIXTURES_DIR = pathlib.Path(__file__).absolute().parent.parent / "fixtures_vcr"
+LP_VCR = VCR(
+    cassette_library_dir=str(FIXTURES_DIR),
+    filter_headers=["Authorization", "user-agent"],
+    path_transformer=VCR.ensure_suffix(".yaml"),
+)
 _CACHE = {}
 
 
@@ -56,10 +81,6 @@ def cache(func: Callable) -> Callable:
 
 
 @unittest.skipIf(IMPORT_ERROR, f"Python module not available: {IMPORT_ERROR}")
-@unittest.skipUnless(
-    "TEST_LAUNCHPAD" in os.environ,
-    "Need Launchpad access with bug control permission. Set TEST_LAUNCHPAD to run.",
-)
 class T(unittest.TestCase):
     # pylint: disable=protected-access,too-many-public-methods
     """Integration tests for apport.crashdb_impl.launchpad."""
@@ -74,6 +95,7 @@ class T(unittest.TestCase):
     #
 
     def setUp(self) -> None:
+        _CACHE.clear()
         self.crashdb = self._get_instance()
 
         # create a local reference report so that we can compare
@@ -83,8 +105,27 @@ class T(unittest.TestCase):
         self.ref_report.add_user_info()
         self.ref_report["SourcePackage"] = "coreutils"
 
-        # Objects tests rely on.
-        self._create_project("langpack-o-matic")
+        self._fake_credentials_if_using_fixtures()
+
+    def _fake_credentials_if_using_fixtures(self):
+        test_id = self.id().rsplit(".", maxsplit=1)[-1]
+        test_fixture = FIXTURES_DIR / f"{test_id}.yaml"
+        if test_fixture.exists():
+            self._set_fake_credentials()
+
+    def _set_fake_credentials(self):
+        if os.path.exists(self.crashdb.auth_file):
+            return
+        self.crashdb._create_auth_dir()
+        print(f"FIXME: writing fake credentials to {self.crashdb.auth_file}")
+        with open(self.crashdb.auth_file, "w", encoding="utf-8") as auth_file:
+            auth_file.write(
+                "[1]\n"
+                "consumer_key = System-wide: Ubuntu (hostname)\n"
+                "consumer_secret = \n"
+                "access_token = FaKET0keN\n"
+                "access_secret = fAk3sEkreT\n"
+            )
 
     def _create_bug_from_report(self, name: str, report: Report) -> int:
         """Create a Launchpad bug report from a crash report.
@@ -193,6 +234,8 @@ and more
 
         return bug.id
 
+    @unittest.skip("FIXME: VCR recording != test env (kernel/DistroRelease)")
+    @LP_VCR.use_cassette
     def test_1_download(self) -> None:
         """download()"""
         r = self.crashdb.download(self.get_segv_report())
@@ -243,6 +286,7 @@ and more
             ),
         )
 
+    @LP_VCR.use_cassette
     def test_2_update_traces(self) -> None:
         # TODO: Split into separate test cases
         # pylint: disable=too-many-statements
@@ -337,6 +381,7 @@ and more
         r["StacktraceSource"] = "a\nb\nc\ne\nf\n\xff\xff\xff\n\f"
         self.crashdb.update_traces(self.get_segv_report(), r, "tests")
 
+    @LP_VCR.use_cassette
     def test_get_comment_url(self) -> None:
         """get_comment_url() for non-ASCII titles"""
         title = b"1\xc3\xa4\xe2\x99\xa52"
@@ -365,6 +410,7 @@ and more
             )
         )
 
+    @LP_VCR.use_cassette
     def test_update_description(self) -> None:
         """update() with changing description"""
         bug_target = self.crashdb.lp_distro.getSourcePackage(name="bash")
@@ -398,6 +444,7 @@ and more
             self.crashdb.launchpad.bugs[crash_id].tags, ["apport-collected"]
         )
 
+    @LP_VCR.use_cassette
     def test_update_comment(self) -> None:
         """update() with appending comment"""
         bug_target = self.crashdb.lp_distro.getSourcePackage(name="bash")
@@ -434,6 +481,7 @@ and more
             self.crashdb.launchpad.bugs[crash_id].tags, ["apport-collected"]
         )
 
+    @LP_VCR.use_cassette
     def test_update_filter(self) -> None:
         """update() with a key filter"""
         bug_target = self.crashdb.lp_distro.getSourcePackage(name="bash")
@@ -472,6 +520,8 @@ and more
 
         self.assertEqual(self.crashdb.launchpad.bugs[crash_id].tags, [])
 
+    @unittest.skip("FIXME: VCR recording != test env (DistroRelease)")
+    @LP_VCR.use_cassette
     def test_get_distro_release(self) -> None:
         """get_distro_release()"""
         self.assertEqual(
@@ -479,6 +529,7 @@ and more
             self.ref_report["DistroRelease"],
         )
 
+    @LP_VCR.use_cassette
     def test_get_affected_packages(self) -> None:
         """get_affected_packages()"""
         self.assertEqual(
@@ -486,16 +537,19 @@ and more
             [self.ref_report["SourcePackage"]],
         )
 
+    @LP_VCR.use_cassette
     def test_is_reporter(self) -> None:
         """is_reporter()"""
         self.assertTrue(self.crashdb.is_reporter(self.get_segv_report()))
         self.assertFalse(self.crashdb.is_reporter(1))
 
+    @LP_VCR.use_cassette
     def test_can_update(self) -> None:
         """can_update()"""
         self.assertTrue(self.crashdb.can_update(self.get_segv_report()))
         self.assertFalse(self.crashdb.can_update(1))
 
+    @LP_VCR.use_cassette
     def test_duplicates(self) -> None:
         """Test duplicate handling."""
         # initially we have no dups
@@ -547,6 +601,8 @@ and more
         self.crashdb.mark_regression(segv_id, known_test_id)
         self._verify_marked_regression(segv_id)
 
+    @unittest.skip("TODO: Figure out why it fails with cassettes.")
+    @LP_VCR.use_cassette
     def test_marking_segv(self) -> None:
         """Test processing status markings for signal crashes."""
         # mark_retraced()
@@ -585,8 +641,10 @@ and more
             self.crashdb.get_fixed_version(self.get_segv_report()), "invalid"
         )
 
+    @LP_VCR.use_cassette
     def test_marking_project(self) -> None:
         """Test processing status markings for a project CrashDB."""
+        self._create_project("langpack-o-matic")
         # create a distro bug
         distro_bug = self.crashdb.launchpad.bugs.createBug(
             description="foo",
@@ -621,6 +679,7 @@ and more
         )
         self.assertIsNone(self.crashdb.get_fixed_version(project_bug.id))
 
+    @LP_VCR.use_cassette
     def test_marking_foreign_arch(self) -> None:
         """Test processing status markings for a project CrashDB."""
         # create a DB for fake arch
@@ -654,6 +713,8 @@ and more
             fakearch_unretraced_after, fakearch_unretraced_before.union(set([bug.id]))
         )
 
+    @unittest.skip("TODO: Figure out why it fails with cassettes.")
+    @LP_VCR.use_cassette
     def test_marking_python(self) -> None:
         """Test processing status markings for interpreter crashes."""
         unchecked_before = self.crashdb.get_dup_unchecked()
@@ -667,6 +728,7 @@ and more
         )
         self.assertIsNone(self.crashdb.get_fixed_version(self.get_python_report()))
 
+    @LP_VCR.use_cassette
     def test_update_traces_invalid(self) -> None:
         """Test updating an invalid crash.
 
@@ -690,6 +752,7 @@ and more
         r = self.crashdb.download(crash_id)
         self.assertNotIn("CoreDump", r)
 
+    @LP_VCR.use_cassette
     @unittest.mock.patch.object(CrashDatabase, "_get_source_version")
     def test_get_fixed_version(self, get_source_version_mock: MagicMock) -> None:
         """get_fixed_version() for fixed bugs
@@ -829,8 +892,10 @@ and more
         bug = self.crashdb.launchpad.bugs[crash_id]
         self.assertIn("regression-retracer", bug.tags)
 
+    @LP_VCR.use_cassette
     def test_project(self):
         """Test reporting crashes against a project instead of a distro."""
+        self._create_project("langpack-o-matic")
         launchpad_instance = os.environ.get("APPORT_LAUNCHPAD_INSTANCE") or "qastaging"
         # crash database for langpack-o-matic project (this does not have
         # packages in any distro)
@@ -886,6 +951,7 @@ NameError: global name 'weird' is not defined"""
         self.assertIsNone(crashdb.duplicate_of(crash_id))
         self.assertIsNone(crashdb.get_fixed_version(crash_id))
 
+    @LP_VCR.use_cassette
     def test_download_robustness(self) -> None:
         """download() of uncommon description formats"""
         # only ProblemType/Architecture/DistroRelease in description
@@ -894,6 +960,7 @@ NameError: global name 'weird' is not defined"""
         self.assertEqual(r["Architecture"], "amd64")
         self.assertTrue(r["DistroRelease"].startswith("Ubuntu "))
 
+    @LP_VCR.use_cassette
     def test_escalation(self):
         """Escalating bugs with more than 10 duplicates"""
         launchpad_instance = os.environ.get("APPORT_LAUNCHPAD_INSTANCE") or "qastaging"
@@ -933,6 +1000,7 @@ NameError: global name 'weird' is not defined"""
                 db.close_duplicate(Report(), bug_id, None)
         sys.stderr.write("\n")
 
+    @LP_VCR.use_cassette
     def test_marking_python_task_mangle(self) -> None:
         """Test source package task fixup for marking interpreter
         scrashes."""
