0de2b5d03fdc075d438056a23aa5a2d4e638869a
Benjamin Drung <benjamin.drung@canonical.com>
2026-04-22 20:29:14 +0200

    apt_dpkg: discard cached contents mapping on EOFError
    test_retrace_jammy_sandbox might fail in a autopkgtest VM:
    
    ```
    Traceback (most recent call last):
      File "/usr/bin/apport-retrace", line 713, in <module>
        sys.exit(main(sys.argv[1:]))
                 ~~~~^^^^^^^^^^^^^^
      File "/usr/bin/apport-retrace", line 542, in main
        sandbox, cache, outdated_msg = apport.sandboxutils.make_sandbox(
                                       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
            report,
            ^^^^^^^
        ...<6 lines>...
            options.dynamic_origins,
            ^^^^^^^^^^^^^^^^^^^^^^^^
        )
        ^
      File "/usr/lib/python3/dist-packages/apport/sandboxutils.py", line 274, in make_sandbox
        pkgs = needed_runtime_packages(report, pkgmap_cache_dir, pkg_versions, verbose)
      File "/usr/lib/python3/dist-packages/apport/sandboxutils.py", line 103, in needed_runtime_packages
        pkg = packaging.get_file_package(
            line,
        ...<3 lines>...
            arch=report.get("Architecture"),
        )
      File "/usr/lib/python3/dist-packages/apport/packaging_impl/apt_dpkg.py", line 800, in get_file_package
        return self._search_contents(file, map_cachedir, release, arch)
               ~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib/python3/dist-packages/apport/packaging_impl/apt_dpkg.py", line 1752, in _search_contents
        contents_mapping = self._get_file2pkg_mapping(map_cachedir, release, arch)
      File "/usr/lib/python3/dist-packages/apport/packaging_impl/apt_dpkg.py", line 1726, in _get_file2pkg_mapping
        file2pkg = self._contents_mapping(map_cachedir, release, arch)
      File "/usr/lib/python3/dist-packages/apport/packaging_impl/apt_dpkg.py", line 340, in _contents_mapping
        self._contents_mapping_obj = pickle.load(fp)
                                     ~~~~~~~~~~~^^^^
    EOFError: Ran out of input
    ```
    
    Discard the cache in those cases and just rebuild them.
    
    Bug: https://launchpad.net/bugs/2149911

--- apport/packaging_impl/apt_dpkg.py
+++ apport/packaging_impl/apt_dpkg.py
@@ -309,7 +309,7 @@ class _AptDpkgPackageInfo(PackageInfo):
             with mapping_file.open("rb") as fp:
                 self._virtual_mapping_obj = pickle.load(fp)
             assert isinstance(self._virtual_mapping_obj, dict)
-        except (AssertionError, FileNotFoundError):
+        except (AssertionError, EOFError, FileNotFoundError, pickle.UnpicklingError):
             self._virtual_mapping_obj = {}
 
         return self._virtual_mapping_obj
@@ -339,7 +339,7 @@ class _AptDpkgPackageInfo(PackageInfo):
             with mapping_file.open("rb") as fp:
                 self._contents_mapping_obj = pickle.load(fp)
             assert isinstance(self._contents_mapping_obj, dict)
-        except (AssertionError, FileNotFoundError):
+        except (AssertionError, EOFError, FileNotFoundError, pickle.UnpicklingError):
             self._contents_mapping_obj = {
                 b"release": release.encode(),
                 b"arch": arch.encode(),
